ed960fa784ddf944e0145e59e482c281222ee550
aaguila <aaguila@lookiero.com>
2019-03-22 12:50:16 +0100

    01-basic done

--- cypress/integration/01-basic/spec.js
+++ cypress/integration/01-basic/spec.js
@@ -6,10 +6,11 @@ it('loads', () => {
 
   // this assertion fails on purpose
   // can you fix it?
-  cy.contains('h1', 'Todos App')
+  cy.contains('h1', 'todos')
 
   // also good practice is to use data attributes specifically for testing
   // see https://on.cypress.io/best-practices#Selecting-Elements
   // which play well with "Selector Playground" tool
   // how would you do select this element?
+  cy.contains('[data-cy=app-title]', 'todos')
 })
